89801678c7023393c25ac3352025e8d28113c5f4
Kevin Ndung'u <kevgathuku@gmail.com>
2017-09-15 18:14:36 +0300

    Add code to reproduce drag error

--- src/App.js
+++ src/App.js
@@ -1,21 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Rect, Group, Stage, Layer } from "react-konva";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            x: 50,
+            y: 50,
+            x1: 100,
+            y1: 100
+        };
+    }
+
+    onDragEnd = evt => {
+        const width = this.state.x1 - this.state.x;
+        const height = this.state.y1 - this.state.y;
+
+        const absolutePosition = this.rectRef.getAbsolutePosition();
+
+        console.log("onDragEnd BoardRect", absolutePosition);
+
+        this.setState({
+            x: absolutePosition.x,
+            y: absolutePosition.y,
+            x1: absolutePosition.x + width,
+            y1: absolutePosition.y + height
+        });
+    };
+    render() {
+        const width = this.state.x1 - this.state.x;
+        const height = this.state.y1 - this.state.y;
+
+        return (
+            <div width={350} height={660}>
+                <Stage
+                    width={350}
+                    height={660}
+                    style={{ border: "1px solid black" }}
+                >
+                    <Layer>
+                        <Group
+                            onMouseOver={this.onMouseOver}
+                            onDragEnd={this.onDragEnd}
+                            draggable
+                        >
+                            <Rect
+                                ref={node => {
+                                    this.rectRef = node;
+                                }}
+                                x={this.state.x}
+                                y={this.state.y}
+                                width={width}
+                                height={height}
+                                stroke="red"
+                                strokeWidth={4}
+                            />
+                        </Group>
+                    </Layer>
+                </Stage>
+            </div>
+        );
+    }
 }
 
 export default App;
